563a771d59211e90754c784d795f9d680c6234e8
Joakim Hellsén <tlovinator@gmail.com>
2023-11-21 00:59:28 +0100

    Fix Ruff warnings and add test

--- main.py
+++ main.py
@@ -1,38 +1,34 @@
+from __future__ import annotations
+
 import os
 import sys
+from typing import TYPE_CHECKING
 
-import requests
 from discord_webhook import DiscordEmbed, DiscordWebhook
-from dotenv import load_dotenv
+from dotenv import find_dotenv, load_dotenv
 from fastapi import FastAPI, HTTPException, Request
+
+if TYPE_CHECKING:
+    import requests
 
 app = FastAPI()
 
 # Take environment variables from .env.
-load_dotenv(verbose=True)
+load_dotenv(find_dotenv(), verbose=True)
 
 # Get the webhook URL.
-WEBHOOK_URL = os.environ.get("WEBHOOK_URL", default=None)
+WEBHOOK_URL: str | None = os.environ.get("WEBHOOK_URL", default=None)
 if not WEBHOOK_URL:
     sys.exit("You need to fill out the .env or add WEBHOOK_URL to your environment.")
 
 
-@app.get("/")
-async def root():
-    """
-    Our index page.
-    """
-    return {"message": "Hello World"}
-
-
 @app.post("/webhook")
 async def webhook(request: Request) -> dict[str, str]:
-    """
-    This is where GitHub will send webhooks to.
+    """This is where GitHub will send webhooks to.
 
     First we check if the x-github-event header is for sponsorships.
-    Then we parse your username, their username, avatar, URL to profile, sponsor tier and then create the message
-    that we will send to Discord.
+    Then we parse your username, their username, avatar, URL to profile, sponsor tier
+    and then create the message that we will send to Discord.
 
     Args:
         request: The HTTP request that contains the JSON from GitHub.
@@ -44,7 +40,7 @@ async def webhook(request: Request) -> dict[str, str]:
     response = await request.json()
 
     # Check if header is correct.
-    await check_header(request)
+    await raise_if_wrong_header(request)
 
     # Send webhook to Discord.
     send_webhook(response)
@@ -53,44 +49,46 @@ async def webhook(request: Request) -> dict[str, str]:
     return {"status": "SUCCESS"}
 
 
-async def check_header(request) -> None:
-    """
-    Check if header is correct.
+async def raise_if_wrong_header(request: Request) -> None:
+    """Check if the header is correct.
 
-    Every webhook from GitHub has a header with the name of the event that triggered the delivery.
+    Every webhook from GitHub has a header with the name of the event that triggered the
+    delivery.
 
     Args:
         request: The HTTP request that contains the JSON from GitHub.
 
     Raises:
         HTTPException: If the header is not "sponsorship".
+        HTTPException: If the header is missing.
     """
-    x_github_event: str = request.headers.get("x-github-event")
+    x_github_event: str | None = request.headers.get("x-github-event")
+    if not x_github_event:
+        raise HTTPException(status_code=500, detail="No header found.")
     if x_github_event != "sponsorship":
         raise HTTPException(status_code=500, detail="Only sponsorships are allowed.")
 
 
-def send_webhook(response, webhook_url=WEBHOOK_URL) -> requests.Response:
-    """
-    Create the webhook and send to Discord.
+def send_webhook(response: dict, webhook_url: str = WEBHOOK_URL) -> requests.Response:
+    """Create the webhook and send to Discord.
 
     Args:
         response: The JSON response from GitHub.
         webhook_url: The URL to the webhook.
     """
     # Your GitHub username.
-    owner_username = response["sponsorship"]["sponsorable"]["login"]
+    owner_username: str = response["sponsorship"]["sponsorable"]["login"]
 
     # Their username, avatar and URL to profile.
-    sponsor_login = response["sponsorship"]["sponsor"]["login"]
-    sponsor_avatar = response["sponsorship"]["sponsor"]["avatar_url"]
-    sponsor_url = response["sponsorship"]["sponsor"]["html_url"]
+    sponsor_login: str = response["sponsorship"]["sponsor"]["login"]
+    sponsor_avatar: str = response["sponsorship"]["sponsor"]["avatar_url"]
+    sponsor_url: str = response["sponsorship"]["sponsor"]["html_url"]
 
     # The name of the sponsor tier, for example '$1 one time'.
-    sponsor_tier_name = response["sponsorship"]["tier"]["name"]
+    sponsor_tier_name: str = response["sponsorship"]["tier"]["name"]
 
     # The message we will send to Discord.
-    message = f"{owner_username} got a sponsor from {sponsor_login}:\n{sponsor_tier_name}"
+    message: str = f"{owner_username} got a sponsor from {sponsor_login}:\n{sponsor_tier_name}"
 
     # Send a webhook to Discord.
     hook = DiscordWebhook(url=webhook_url, rate_limit_retry=True, username="GitHub sponsors")
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,91 +1,157 @@
-import os
+from __future__ import annotations
 
+import os
+from typing import TYPE_CHECKING, cast
+
+import pytest
 from dotenv import load_dotenv
+from fastapi import HTTPException, Request
 from starlette.testclient import TestClient
 
-from main import app, send_webhook
+from main import app, raise_if_wrong_header, send_webhook
+
+if TYPE_CHECKING:
+    from requests import Response
 
 client = TestClient(app)
-json_boi = {'action': 'created',
-            'sponsorship': {'node_id': 'S_kwDAX"OAD9fc84ASDAasf', 'created_at': '2022-11-23T12:57:58+00:00',
-                            'sponsorable': {'login': 'TheLovinator1', 'id': 4153203, 'node_id': 'MDQ61asXasjQxNTMyMDM=',
-                                            'avatar_url': 'https://avatars.githubusercontent.com/u/4153203?v=4',
-                                            'gravatar_id': '', 'url': 'https://api.github.com/users/TheLovinator1',
-                                            'html_url': 'https://github.com/TheLovinator1',
-                                            'followers_url': 'https://api.github.com/users/TheLovinator1/followers',
-                                            'following_url': 'https://api.github.com/users/TheLovinator1/following{'
-                                                             '/other_user}',
-                                            'gists_url': 'https://api.github.com/users/TheLovinator1/gists{/gist_id}',
-                                            'starred_url': 'https://api.github.com/users/TheLovinator1/starred{'
-                                                           '/owner}{/repo}',
-                                            'subscriptions_url':
-                                                'https://api.github.com/users/TheLovinator1/subscriptions',
-                                            'organizations_url': 'https://api.github.com/users/TheLovinator1/orgs',
-                                            'repos_url': 'https://api.github.com/users/TheLovinator1/repos',
-                                            'events_url': 'https://api.github.com/users/TheLovinator1/events{/privacy}',
-                                            'received_events_url':
-                                                'https://api.github.com/users/TheLovinator1/received_events',
-                                            'type': 'User', 'site_admin': False},
-                            'maintainer': {'login': 'TheLovinator1', 'id': 4153203, 'node_id': 'MDQ61asXasjQxNTMyMDM=',
-                                           'avatar_url': 'https://avatars.githubusercontent.com/u/4153203?v=4',
-                                           'gravatar_id': '', 'url': 'https://api.github.com/users/TheLovinator1',
-                                           'html_url': 'https://github.com/TheLovinator1',
-                                           'followers_url': 'https://api.github.com/users/TheLovinator1/followers',
-                                           'following_url': 'https://api.github.com/users/TheLovinator1/following{'
-                                                            '/other_user}',
-                                           'gists_url': 'https://api.github.com/users/TheLovinator1/gists{/gist_id}',
-                                           'starred_url': 'https://api.github.com/users/TheLovinator1/starred{'
-                                                          '/owner}{/repo}',
-                                           'subscriptions_url':
-                                               'https://api.github.com/users/TheLovinator1/subscriptions',
-                                           'organizations_url': 'https://api.github.com/users/TheLovinator1/orgs',
-                                           'repos_url': 'https://api.github.com/users/TheLovinator1/repos',
-                                           'events_url': 'https://api.github.com/users/TheLovinator1/events{/privacy}',
-                                           'received_events_url':
-                                               'https://api.github.com/users/TheLovinator1/received_events',
-                                           'type': 'User', 'site_admin': False},
-                            'sponsor': {'login': 'testing', 'id': 1, 'node_id': 'U_kgDOBwX9RA',
-                                        'avatar_url': 'https://avatars.githubusercontent.com/u/1?v=4',
-                                        'gravatar_id': '', 'url': 'https://api.github.com/users/testing',
-                                        'html_url': 'https://github.com/testing',
-                                        'followers_url': 'https://api.github.com/users/testing/followers',
-                                        'following_url': 'https://api.github.com/users/testing/following{/other_user}',
-                                        'gists_url': 'https://api.github.com/users/testing/gists{/gist_id}',
-                                        'starred_url': 'https://api.github.com/users/testing/starred{/owner}{/repo}',
-                                        'subscriptions_url': 'https://api.github.com/users/testing/subscriptions',
-                                        'organizations_url': 'https://api.github.com/users/testing/orgs',
-                                        'repos_url': 'https://api.github.com/users/testing/repos',
-                                        'events_url': 'https://api.github.com/users/testing/events{/privacy}',
-                                        'received_events_url': 'https://api.github.com/users/testing/received_events',
-                                        'type': 'User', 'site_admin': False}, 'privacy_level': 'public',
-                            'tier': {'node_id': 'ST_kwDOAD9fc84AAiB5', 'created_at': '2022-03-02T23:11:37Z',
-                                     'description': '- Buy me coffee.\r\n\r\nMore coffee = more code.\r\n',
-                                     'monthly_price_in_cents': 100, 'monthly_price_in_dollars': 1,
-                                     'name': '$1 one time', 'is_one_time': True, 'is_custom_amount': False}},
-            'sender': {'login': 'testing', 'id': 1, 'node_id': 'U_kasfafwX9RA',
-                       'avatar_url': 'https://avatars.githubusercontent.com/u/1?v=4', 'gravatar_id': '',
-                       'url': 'https://api.github.com/users/testing', 'html_url': 'https://github.com/testing',
-                       'followers_url': 'https://api.github.com/users/testing/followers',
-                       'following_url': 'https://api.github.com/users/testing/following{/other_user}',
-                       'gists_url': 'https://api.github.com/users/testing/gists{/gist_id}',
-                       'starred_url': 'https://api.github.com/users/testing/starred{/owner}{/repo}',
-                       'subscriptions_url': 'https://api.github.com/users/testing/subscriptions',
-                       'organizations_url': 'https://api.github.com/users/testing/orgs',
-                       'repos_url': 'https://api.github.com/users/testing/repos',
-                       'events_url': 'https://api.github.com/users/testing/events{/privacy}',
-                       'received_events_url': 'https://api.github.com/users/testing/received_events', 'type': 'User',
-                       'site_admin': False}}
+json_boi = {
+    "action": "created",
+    "sponsorship": {
+        "node_id": 'S_kwDAX"OAD9fc84ASDAasf',
+        "created_at": "2022-11-23T12:57:58+00:00",
+        "sponsorable": {
+            "login": "TheLovinator1",
+            "id": 4153203,
+            "node_id": "MDQ61asXasjQxNTMyMDM=",
+            "avatar_url": "https://avatars.githubusercontent.com/u/4153203?v=4",
+            "gravatar_id": "",
+            "url": "https://api.github.com/users/TheLovinator1",
+            "html_url": "https://github.com/TheLovinator1",
+            "followers_url": "https://api.github.com/users/TheLovinator1/followers",
+            "following_url": "https://api.github.com/users/TheLovinator1/following{" "/other_user}",
+            "gists_url": "https://api.github.com/users/TheLovinator1/gists{/gist_id}",
+            "starred_url": "https://api.github.com/users/TheLovinator1/starred{" "/owner}{/repo}",
+            "subscriptions_url": "https://api.github.com/users/TheLovinator1/subscriptions",
+            "organizations_url": "https://api.github.com/users/TheLovinator1/orgs",
+            "repos_url": "https://api.github.com/users/TheLovinator1/repos",
+            "events_url": "https://api.github.com/users/TheLovinator1/events{/privacy}",
+            "received_events_url": "https://api.github.com/users/TheLovinator1/received_events",
+            "type": "User",
+            "site_admin": False,
+        },
+        "maintainer": {
+            "login": "TheLovinator1",
+            "id": 4153203,
+            "node_id": "MDQ61asXasjQxNTMyMDM=",
+            "avatar_url": "https://avatars.githubusercontent.com/u/4153203?v=4",
+            "gravatar_id": "",
+            "url": "https://api.github.com/users/TheLovinator1",
+            "html_url": "https://github.com/TheLovinator1",
+            "followers_url": "https://api.github.com/users/TheLovinator1/followers",
+            "following_url": "https://api.github.com/users/TheLovinator1/following{" "/other_user}",
+            "gists_url": "https://api.github.com/users/TheLovinator1/gists{/gist_id}",
+            "starred_url": "https://api.github.com/users/TheLovinator1/starred{" "/owner}{/repo}",
+            "subscriptions_url": "https://api.github.com/users/TheLovinator1/subscriptions",
+            "organizations_url": "https://api.github.com/users/TheLovinator1/orgs",
+            "repos_url": "https://api.github.com/users/TheLovinator1/repos",
+            "events_url": "https://api.github.com/users/TheLovinator1/events{/privacy}",
+            "received_events_url": "https://api.github.com/users/TheLovinator1/received_events",
+            "type": "User",
+            "site_admin": False,
+        },
+        "sponsor": {
+            "login": "testing",
+            "id": 1,
+            "node_id": "U_kgDOBwX9RA",
+            "avatar_url": "https://avatars.githubusercontent.com/u/1?v=4",
+            "gravatar_id": "",
+            "url": "https://api.github.com/users/testing",
+            "html_url": "https://github.com/testing",
+            "followers_url": "https://api.github.com/users/testing/followers",
+            "following_url": "https://api.github.com/users/testing/following{/other_user}",
+            "gists_url": "https://api.github.com/users/testing/gists{/gist_id}",
+            "starred_url": "https://api.github.com/users/testing/starred{/owner}{/repo}",
+            "subscriptions_url": "https://api.github.com/users/testing/subscriptions",
+            "organizations_url": "https://api.github.com/users/testing/orgs",
+            "repos_url": "https://api.github.com/users/testing/repos",
+            "events_url": "https://api.github.com/users/testing/events{/privacy}",
+            "received_events_url": "https://api.github.com/users/testing/received_events",
+            "type": "User",
+            "site_admin": False,
+        },
+        "privacy_level": "public",
+        "tier": {
+            "node_id": "ST_kwDOAD9fc84AAiB5",
+            "created_at": "2022-03-02T23:11:37Z",
+            "description": "- Buy me coffee.\r\n\r\nMore coffee = more code.\r\n",
+            "monthly_price_in_cents": 100,
+            "monthly_price_in_dollars": 1,
+            "name": "$1 one time",
+            "is_one_time": True,
+            "is_custom_amount": False,
+        },
+    },
+    "sender": {
+        "login": "testing",
+        "id": 1,
+        "node_id": "U_kasfafwX9RA",
+        "avatar_url": "https://avatars.githubusercontent.com/u/1?v=4",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/testing",
+        "html_url": "https://github.com/testing",
+        "followers_url": "https://api.github.com/users/testing/followers",
+        "following_url": "https://api.github.com/users/testing/following{/other_user}",
+        "gists_url": "https://api.github.com/users/testing/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/testing/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/testing/subscriptions",
+        "organizations_url": "https://api.github.com/users/testing/orgs",
+        "repos_url": "https://api.github.com/users/testing/repos",
+        "events_url": "https://api.github.com/users/testing/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/testing/received_events",
+        "type": "User",
+        "site_admin": False,
+    },
+}
 
 
-def test_send_webhook():
+def test_send_webhook() -> None:
+    """Test if webhook is sent without errors."""
     load_dotenv(verbose=True)
-    webhook_url = os.environ.get("WEBHOOK_URL", default=None)
-    hook = send_webhook(json_boi, webhook_url)
+    webhook_url: str | None = os.environ.get("WEBHOOK_URL", default=None)
+    if not webhook_url:
+        msg = "No webhook URL found."
+        raise ValueError(msg)
+
+    hook: Response = send_webhook(json_boi, webhook_url)
     assert hook.ok
-    assert hook.status_code == 200
 
 
-def test_root():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+@pytest.mark.asyncio()
+async def test_check_header() -> None:
+    """Test if check_header works."""
+
+    class DummyRequest:
+        # Create a dummy Request object with the necessary headers
+        def __init__(self: DummyRequest, headers: dict[str, str]) -> None:
+            self.headers = headers
+
+    # Test case 1: Correct header ("sponsorship")
+    request_correct = DummyRequest(headers={"x-github-event": "sponsorship"})
+    request_correct = cast(Request, request_correct)
+    await raise_if_wrong_header(request_correct)  # This should not raise any exceptions
+
+    # Test case 2: Missing header
+    request_missing = DummyRequest(headers={})
+    request_missing = cast(Request, request_missing)
+    with pytest.raises(HTTPException) as exc_info:
+        await raise_if_wrong_header(request_missing)
+    assert exc_info.value.status_code == 500  # noqa: PLR2004
+    assert exc_info.value.detail == "No header found."
+
+    # Test case 3: Incorrect header ("push" instead of "sponsorship")
+    request_incorrect = DummyRequest(headers={"x-github-event": "push"})
+    request_incorrect = cast(Request, request_incorrect)
+    with pytest.raises(HTTPException) as exc_info:
+        await raise_if_wrong_header(request_incorrect)
+    assert exc_info.value.status_code == 500  # noqa: PLR2004
+    assert exc_info.value.detail == "Only sponsorships are allowed."
